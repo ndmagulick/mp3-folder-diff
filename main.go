2c7bfd745c4d46e576eb50f913e88c0a5adfd98b
Nick <ndmagulick@gmail.com>
2025-10-02 00:07:34 +0200

    Start messing with reading directories

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+	readDirectory()
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readDirectory() {
+	directoryEntries, err := os.ReadDir("")
+	check(err)
+
+	fmt.Println("Listing subdir/parent")
+	for _, entry := range directoryEntries {
+		if !entry.IsDir() {
+			fmt.Println(" ", entry.Name())
+		}
+	}
 }
