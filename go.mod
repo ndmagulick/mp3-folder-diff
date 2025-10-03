9554cc57930ffd7b1581df7d8d5f3cab05ee5c1b
Nick <ndmagulick@gmail.com>
2025-10-03 23:05:09 +0200

    Add some file functions, add todos, import gopsutil

---
+++ go.mod
@@ -0,0 +1,13 @@
+module mp3-folder-diff
+
+go 1.25.1
+
+require github.com/shirou/gopsutil/v3 v3.24.5
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/shirou/gopsutil/v3/disk"
 )
 
 // TODO - use maps instead of slices since file names are unique, and would improve efficency
 func main() {
 	const sourceDirectory string = ""
 	const destinationDirectory string = ""
+
+	diskSizeStub()
 
 	var sourceDirectoryEntries []string = readDirectory(sourceDirectory)
 	printEntries(sourceDirectoryEntries)
@@ -25,12 +30,14 @@ func main() {
 	printEntries(filesToBeAdded)
 }
 
+// TODO handle errors better
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+// TODO move these to another file
 func readDirectory(sourceDirectory string) []string {
 	directoryEntries, err := os.ReadDir(sourceDirectory)
 	check(err)
@@ -71,4 +78,57 @@ func contains(slice []string, value string) bool {
 	}
 
 	return false
+}
+
+// TODO use full path for files, using string just for conceptual purposes
+func deleteFiles(files []string) {
+	for _, file := range files {
+		error := os.Remove(file)
+		check(error)
+	}
+}
+
+func copyFiles(filesToCopy []string, destinationPath string) {
+	for _, file := range filesToCopy {
+		err := copyFile(file, destinationPath)
+		if err != nil {
+			fmt.Println("Error copying file:", file, ":", err)
+		} else {
+			fmt.Println("Copied file", file, " successfully")
+		}
+	}
+}
+
+func copyFile(fileToCopy string, destinationPath string) error {
+	// check directory exists, do later
+
+	sourceFile, err := os.Open(fileToCopy)
+	check(err)
+
+	defer sourceFile.Close()
+
+	// will append file name at the end of the path
+	destinationFile, err := os.Create(destinationPath)
+	check(err)
+
+	defer destinationFile.Close()
+
+	_, err = io.Copy(sourceFile, destinationFile)
+	check(err)
+
+	return destinationFile.Sync()
+}
+
+// TODO - use these functions to calculate size remaining on disk for checks when copying
+func diskSizeStub() {
+	usageStat, err := disk.Usage("/")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Disk Path: %s\n", usageStat.Path)
+	fmt.Printf("Total: %.2f GB\n", float64(usageStat.Total)/1e9)
+	fmt.Printf("Free: %.2f GB\n", float64(usageStat.Free)/1e9)
+	fmt.Printf("Used: %.2f GB\n", float64(usageStat.Used)/1e9)
+	fmt.Printf("Used Percent: %.2f%%\n", usageStat.UsedPercent)
 }
